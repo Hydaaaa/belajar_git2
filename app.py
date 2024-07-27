d64cfed74e8dc65adee9c4db54a33f76d642a4db
hydaaaa <hidayatullahrohmad510@gmail.com>
2024-07-27 14:28:53 +0700

    Penambahan feature dari branch developer, fixing bug

---
+++ app.py
@@ -0,0 +1,9 @@
+def calculator():
+    num1 = float(input("Enter the first number": ""))
+    num2 = float(input("Enter the second number": ""))
+    return num1 + num2
+
+def calculator():
+    num1 = float(input("Enter the first number": ""))
+    num2 = float(input("Enter the second number": ""))
+    print (f"The sum of the two number is : {num1 + num2}")
